dd89a33ad9ebb040ca300e2570491b9e45127d37
LevBernstein <lev2000@me.com>
2021-06-11 18:49:47 -0400

    Placeholder gui text

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
 import Vid from './utils/Camera.js'
-import GUI from './utils/Gui.js'
+import Interface from './utils/Gui.js'
 
 export default function App() {
   const algRef = useRef('101001') //TODO: algorithm encoded based on "prescription"
+  const [showGUI, setShowGUI] = useState(true)
+
+  const handleClick = () => {
+    algRef.current += 1
+    console.log(algRef.current)
+    setShowGUI(!showGUI)
+  }
+
+  useEffect(() => {
+    handleClick()
+  }, [algRef])
 
   return (
     <View style={styles.container}>
       <Text style={styles.Text}></Text>
       <StatusBar style="auto" />
-      <div onClick={GUI}>
+      <div onClick={handleClick}>
         <Vid style={{ display: 'block' }} Algorithm={algRef.current} />
+      </div>
+      <div className="Interface">
+        <Interface disp={showGUI} />
       </div>
     </View>
   )
@@ -21,7 +35,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    margin: 0,
     backgroundColor: '#F1F1F1',
     alignItems: 'center',
     justifyContent: 'center'
--- utils/Gui.js
+++ utils/Gui.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { useEffect, useRef } from 'react'
 
-const GUI = (props) => {
-  console.log('e')
+const Interface = (props) => {
+  console.log('disp: ' + props.disp)
+  if (props.disp == false) {
+    return <h1></h1>
+  }
+  return <h1>GUI placeholder</h1>
 }
 
-export default GUI
+export default Interface
