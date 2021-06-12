219058f9ea008ea6526254cf067c75866a029a82
Lev Bernstein <10897595+LevBernstein@users.noreply.github.com>
2021-06-12 10:47:48 -0400

    Platform detection (#18)

--- App.js
+++ App.js
@@ -2,31 +2,29 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { useEffect, useRef, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import getPlatform from './utils/Platform.js'
 import Vid from './utils/Camera.js'
 import GUI from './utils/Gui.js'
 
 export default function App() {
-  const algRef = useRef('101001') //TODO: algorithm encoded based on "prescription"
-  const [showGUI, setShowGUI] = useState(true)
-
+  const [alg, setAlg] = useState('10101') //TODO: algorithm encoded based on "prescription"
+  const [showGUI, setShowGUI] = useState(false)
+  const platform = getPlatform()
+  console.log(platform)
   const handleClick = () => {
-    algRef.current += 1
-    console.log(algRef.current)
+    setAlg(alg + 1)
+    console.log(alg)
     setShowGUI(!showGUI)
   }
-
-  useEffect(() => {
-    handleClick()
-  }, [algRef])
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <div onClick={handleClick}>
-        <Vid Algorithm={algRef.current} />
+        <Vid Algorithm={alg} Platform={platform} />
       </div>
-      <div className="Interface">
-        <GUI disp={showGUI} />
+      <div className="Interface" style={{ display: showGUI ? '' : 'none' }}>
+        <GUI Disp={showGUI} Platform={platform} />
       </div>
     </View>
   )
