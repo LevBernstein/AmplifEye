8b15090e14e3f96bdeda4e37a8fca22bc7ee83d2
LevBernstein <lev2000@me.com>
2021-06-16 09:43:36 -0400

    folder structure

--- front-end/App.js
+++ front-end/App.js
@@ -3,8 +3,7 @@ import React from 'react'
 import { useEffect, useRef, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import getPlatform from './utils/Platform.js'
-import Vid from './utils/Camera.js'
-import GUI from './utils/Gui.js'
+import Vid from './components/Camera.js'
 
 export default function App() {
   const [alg, setAlg] = useState('10101') //TODO: algorithm encoded based on "prescription"
@@ -17,14 +16,29 @@ export default function App() {
     setShowGUI(!showGUI)
   }
 
+  const GUI = (props) => {
+    if (!props.Disp) {
+      return <></>
+    }
+    return (
+      <>
+        <h1>j</h1> <p>e</p>
+      </>
+    )
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <div onClick={handleClick}>
-        <Vid Algorithm={alg} Platform={platform} />
-      </div>
       <div className="Interface" style={{ display: showGUI ? '' : 'none' }}>
         <GUI Disp={showGUI} Platform={platform} />
+      </div>
+      <div
+        className="Video"
+        onClick={handleClick}
+        Disp={showGUI}
+        style={{ display: showGUI ? 'none' : '' }}>
+        <Vid Algorithm={alg} Platform={platform} />
       </div>
     </View>
   )
