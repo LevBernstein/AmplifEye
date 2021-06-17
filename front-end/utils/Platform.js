8abebf959166be65956bed27ecb9a9ea914dfb39
Lev Bernstein <10897595+LevBernstein@users.noreply.github.com>
2021-06-17 17:34:21 -0400

    prettier tab change (#28)

--- front-end/utils/Platform.js
+++ front-end/utils/Platform.js
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native'
 
 export default function getPlatform() {
-  const platform =
-    Platform.OS == 'android' || Platform.OS == 'ios' ? 'Mobile' : 'Other'
-  return platform
+	const platform =
+		Platform.OS == 'android' || Platform.OS == 'ios' ? 'Mobile' : 'Other'
+	return platform
 }
