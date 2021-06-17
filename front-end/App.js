f22dbb6885b5c89394fffc7cf1b4f9c62e882a73
LevBernstein <lev2000@me.com>
2021-06-16 21:36:58 -0400

    alg onChange works

--- front-end/App.js
+++ front-end/App.js
@@ -6,83 +6,67 @@ import getPlatform from './utils/Platform.js'
 import Vid from './components/Camera.js'
 
 export default function App() {
-  const [alg, setAlg] = useState('10101') //TODO: algorithm encoded based on "prescription"
-
+  const [alg, setAlg] = useState('10') //TODO: algorithm encoded based on "prescription"
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const [showGUI, setShowGUI] = useState(false)
   const platform = getPlatform()
   console.log(platform)
+
+  const handleSubmit = (event) => {
+    setShowGUI(!showGUI)
+    event.preventDefault()
+  }
 
   const handleClick = () => {
     console.log(alg)
     setShowGUI(!showGUI)
   }
 
-  const GUI = (props) => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const handleSubmit = (event) => {
-      props.setDisp(!props.disp)
-      event.preventDefault()
-    }
-
-    return (
-      <>
-        {!props.disp ? (
-          <></>
-        ) : (
-          <div>
-            <form id="guiForm" onSubmit={handleSubmit}>
-              <label className="guiLabel">Email: </label>
-              <input
-                className="inputs"
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <br />
-              <label className="guiLabel">Password: </label>
-              <input
-                className="inputs"
-                type="password"
-                id="password"
-                name="password"
-                placeholder="******"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <br />
-              <label className="guiLabel">Algorithm: </label>
-              <input
-                className="inputs"
-                type="text"
-                id="alg"
-                name="alg"
-                value={props.alg}
-                onChange={(e) => props.setAlg(e.target.value)}
-              />
-              <button type="submit" className="prof-button" form="guiForm">
-                Save
-              </button>
-            </form>
-          </div>
-        )}
-      </>
-    )
-  }
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <div className="Interface" style={{ display: showGUI ? '' : 'none' }}>
-        <GUI
-          disp={showGUI}
-          setDisp={setShowGUI}
-          alg={alg}
-          setAlg={setAlg}
-          platform={platform}
-        />
+        {!showGUI ? (
+          <></>
+        ) : (
+          <form id="guiForm" onSubmit={handleSubmit}>
+            <label className="guiLabel"> Email: </label>
+            <input
+              className="Email"
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <label className="guiLabel"> Password: </label>
+            <input
+              className="Password"
+              type="password"
+              id="password"
+              name="password"
+              placeholder="******"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <label className="guiLabel"> Algorithm: </label>
+            <input
+              className="inputs"
+              type="text"
+              id="alg"
+              name="alg"
+              value={alg}
+              onChange={(e) => setAlg(e.target.value)}
+            />
+            <br />
+            <button type="submit" className="profButton" form="guiForm">
+              Save
+            </button>
+          </form>
+        )}
       </div>
       <div
         className="Video"
