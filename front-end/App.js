bcd739be2b293e585c9da14d6609cc445c89ed07
LevBernstein <lev2000@me.com>
2021-06-17 17:33:27 -0400

    prettier tab change

--- front-end/App.js
+++ front-end/App.js
@@ -2,85 +2,85 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { useEffect, useRef, useState } from 'react'
 import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  SafeAreaView
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Button,
+	SafeAreaView
 } from 'react-native'
 import getPlatform from './utils/Platform.js'
 import Vid from './components/Camera.js'
 
 export default function App() {
-  const [alg, setAlg] = useState('0') //TODO: algorithm encoded based on "prescription", re-render video with changed alg.
-  //Need to force an update of Vid or find a more elegant solution
-  const [email, setEmail] = useState('')
-  const [pass, setPass] = useState('')
-  const [showGUI, setShowGUI] = useState(false)
-  const platform = getPlatform()
-  console.log(platform)
+	const [alg, setAlg] = useState('0') //TODO: algorithm encoded based on "prescription", re-render video with changed alg.
+	//Need to force an update of Vid or find a more elegant solution
+	const [email, setEmail] = useState('')
+	const [pass, setPass] = useState('')
+	const [showGUI, setShowGUI] = useState(false)
+	const platform = getPlatform()
+	console.log(platform)
 
-  const handleSubmit = (event) => {
-    setShowGUI(!showGUI)
-    event.preventDefault()
-  }
+	const handleSubmit = (event) => {
+		setShowGUI(!showGUI)
+		event.preventDefault()
+	}
 
-  const handleClick = () => {
-    console.log(alg)
-    setShowGUI(!showGUI)
-  }
+	const handleClick = () => {
+		console.log(alg)
+		setShowGUI(!showGUI)
+	}
 
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={{ display: showGUI ? '' : 'none' }}>
-        <View>
-          <Text>{'\n'}Email: </Text>
-          <TextInput
-            autoCompleteType="email"
-            autoCorrect={false}
-            autoFocus={true}
-            textContentType="emailAddress"
-            placeholder="name@address.com"
-            value={email}
-            onChangeText={setEmail}
-          />
-        </View>
-        <View>
-          <Text>{'\n'}Password: </Text>
-          <TextInput
-            autoCompleteType="password"
-            autoCorrect={false}
-            textContentType="password"
-            secureTextEntry={true}
-            placeholder="*******"
-            value={pass}
-            onChangeText={setPass}
-          />
-        </View>
-        <View>
-          <Text>{'\n'}Algorithm: </Text>
-          <TextInput autoCorrect={false} value={alg} onChangeText={setAlg} />
-        </View>
-        <Text>{'\n'}</Text>
-        <Button title="Save" onPress={(e) => handleSubmit(e)} />
-      </View>
-      <View
-        className="Video"
-        onClick={handleClick}
-        style={{ display: showGUI ? 'none' : '' }}>
-        <Vid disp={showGUI} Algorithm={alg} Platform={platform} />
-      </View>
-    </View>
-  )
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<View style={{ display: showGUI ? '' : 'none' }}>
+				<View>
+					<Text>{'\n'}Email: </Text>
+					<TextInput
+						autoCompleteType="email"
+						autoCorrect={false}
+						autoFocus={true}
+						textContentType="emailAddress"
+						placeholder="name@address.com"
+						value={email}
+						onChangeText={setEmail}
+					/>
+				</View>
+				<View>
+					<Text>{'\n'}Password: </Text>
+					<TextInput
+						autoCompleteType="password"
+						autoCorrect={false}
+						textContentType="password"
+						secureTextEntry={true}
+						placeholder="*******"
+						value={pass}
+						onChangeText={setPass}
+					/>
+				</View>
+				<View>
+					<Text>{'\n'}Algorithm: </Text>
+					<TextInput autoCorrect={false} value={alg} onChangeText={setAlg} />
+				</View>
+				<Text>{'\n'}</Text>
+				<Button title="Save" onPress={(e) => handleSubmit(e)} />
+			</View>
+			<View
+				className="Video"
+				onClick={handleClick}
+				style={{ display: showGUI ? 'none' : '' }}>
+				<Vid disp={showGUI} Algorithm={alg} Platform={platform} />
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    margin: 0,
-    backgroundColor: '#F1F1F1',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+	container: {
+		margin: 0,
+		backgroundColor: '#F1F1F1',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 })
--- front-end/components/Camera.js
+++ front-end/components/Camera.js
@@ -6,113 +6,113 @@ import { Video, AVPlaybackStatus } from 'expo-av'
 import { Camera } from 'expo-camera'
 
 const Vid = (props) => {
-  const Algorithm = parseInt(props.Algorithm, 10)
-  const Plat = props.Platform
-  console.log('Algorithm: ' + Algorithm)
+	const Algorithm = parseInt(props.Algorithm, 10)
+	const Plat = props.Platform
+	console.log('Algorithm: ' + Algorithm)
 
-  if (Plat != 'Mobile') {
-    //Web
-    const constraints = {
-      video: {
-        facingMode: 'environment',
-        framerate: { ideal: 60, max: 120 },
-        width: { ideal: 4096 },
-        height: { ideal: 2160 } //kind of hacky but uses ideal to force the video to display in the camera's max resolution
-      }
-    }
-    const videoRef = useRef(null)
-    const photoRef = useRef(null)
-    useEffect(() => {
-      getVideo(constraints)
-    }, [videoRef])
+	if (Plat != 'Mobile') {
+		//Web
+		const constraints = {
+			video: {
+				facingMode: 'environment',
+				framerate: { ideal: 60, max: 120 },
+				width: { ideal: 4096 },
+				height: { ideal: 2160 } //kind of hacky but uses ideal to force the video to display in the camera's max resolution
+			}
+		}
+		const videoRef = useRef(null)
+		const photoRef = useRef(null)
+		useEffect(() => {
+			getVideo(constraints)
+		}, [videoRef])
 
-    const getVideo = (constraints) => {
-      navigator.mediaDevices.getUserMedia =
-        navigator.mediaDevices.getUserMedia ||
-        navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia ||
-        navigator.oGetUserMedia ||
-        navigator.msGetUserMedia // for compatibility's sake, if navigator.mediaDevices.getUserMedia is not supported, try older versions
-      navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then((stream) => {
-          let video = videoRef.current
-          video.srcObject = stream
-          video.play()
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    }
+		const getVideo = (constraints) => {
+			navigator.mediaDevices.getUserMedia =
+				navigator.mediaDevices.getUserMedia ||
+				navigator.webkitGetUserMedia ||
+				navigator.mozGetUserMedia ||
+				navigator.oGetUserMedia ||
+				navigator.msGetUserMedia // for compatibility's sake, if navigator.mediaDevices.getUserMedia is not supported, try older versions
+			navigator.mediaDevices
+				.getUserMedia(constraints)
+				.then((stream) => {
+					let video = videoRef.current
+					video.srcObject = stream
+					video.play()
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+		}
 
-    const Paint = (alg) => {
-      let video = videoRef.current
-      let photo = photoRef.current
-      let ctx = photo.getContext('2d')
-      const width = video.videoWidth
-      const height = video.videoHeight
-      photo.width = width
-      photo.height = height
+		const Paint = (alg) => {
+			let video = videoRef.current
+			let photo = photoRef.current
+			let ctx = photo.getContext('2d')
+			const width = video.videoWidth
+			const height = video.videoHeight
+			photo.width = width
+			photo.height = height
 
-      return setInterval(() => {
-        ctx.drawImage(video, 0, 0, width, height)
-        let pixels = ctx.getImageData(0, 0, width, height)
-        let l = pixels.data.length / 4
-        for (var i = 0; i < l; i++) {
-          const red = i * 4
-          const green = i * 4 + 1
-          const blue = i * 4 + 2
-          let newColors = Shift(
-            alg,
-            pixels.data[red],
-            pixels.data[green],
-            pixels.data[blue]
-          )
-          pixels.data[red] = newColors[0]
-          pixels.data[green] = newColors[1]
-          pixels.data[blue] = newColors[2]
-        }
+			return setInterval(() => {
+				ctx.drawImage(video, 0, 0, width, height)
+				let pixels = ctx.getImageData(0, 0, width, height)
+				let l = pixels.data.length / 4
+				for (var i = 0; i < l; i++) {
+					const red = i * 4
+					const green = i * 4 + 1
+					const blue = i * 4 + 2
+					let newColors = Shift(
+						alg,
+						pixels.data[red],
+						pixels.data[green],
+						pixels.data[blue]
+					)
+					pixels.data[red] = newColors[0]
+					pixels.data[green] = newColors[1]
+					pixels.data[blue] = newColors[2]
+				}
 
-        ctx.putImageData(pixels, 0, 0)
-      }, 1 / video.framerate) //interval of 16 = 60 fps, good baseline
-    }
+				ctx.putImageData(pixels, 0, 0)
+			}, 1 / video.framerate) //interval of 16 = 60 fps, good baseline
+		}
 
-    return (
-      <View>
-        <video
-          ref={videoRef}
-          onCanPlay={() => Paint(Algorithm)}
-          style={
-            { display: 'none' } //extremely inefficient! TODO: find a way around this
-          }
-        />
-        <canvas ref={photoRef} />
-      </View>
-    )
-  } else {
-    //Mobile:
-    const [hasPermission, setHasPermission] = useState(null)
-    const [type, setType] = useState(Camera.Constants.Type.back)
+		return (
+			<View>
+				<video
+					ref={videoRef}
+					onCanPlay={() => Paint(Algorithm)}
+					style={
+						{ display: 'none' } //extremely inefficient! TODO: find a way around this
+					}
+				/>
+				<canvas ref={photoRef} />
+			</View>
+		)
+	} else {
+		//Mobile:
+		const [hasPermission, setHasPermission] = useState(null)
+		const [type, setType] = useState(Camera.Constants.Type.back)
 
-    useEffect(() => {
-      ;(async () => {
-        const { status } = await Camera.requestPermissionsAsync()
-        setHasPermission(status === 'granted')
-      })()
-    }, [])
+		useEffect(() => {
+			;(async () => {
+				const { status } = await Camera.requestPermissionsAsync()
+				setHasPermission(status === 'granted')
+			})()
+		}, [])
 
-    if (hasPermission === null) {
-      return <View />
-    }
-    if (hasPermission === false) {
-      return <Text>No access to camera</Text>
-    }
-    return (
-      <View>
-        <Camera type={type}></Camera>
-      </View>
-    )
-  }
+		if (hasPermission === null) {
+			return <View />
+		}
+		if (hasPermission === false) {
+			return <Text>No access to camera</Text>
+		}
+		return (
+			<View>
+				<Camera type={type}></Camera>
+			</View>
+		)
+	}
 }
 
 export default Vid
