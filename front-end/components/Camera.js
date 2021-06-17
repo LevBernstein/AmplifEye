b767cabc941fb3186906a25844ce4de97c2085b1
LevBernstein <lev2000@me.com>
2021-06-17 17:02:11 -0400

    Separate returns for mobile and web

--- front-end/components/Camera.js
+++ front-end/components/Camera.js
@@ -3,90 +3,117 @@ import { useEffect, useState, useRef, useCallback } from 'react'
 import Shift from '../utils/Shift.js'
 import { View } from 'react-native'
 //import Canvas from 'react-native-canvas' // has error
-//import { Video, AVPlaybackStatus } from 'expo-av' // does not paint with Paint()
-//TODO: switch to react-native versions of video, canvas
+import { Video, AVPlaybackStatus } from 'expo-av' // does not paint with Paint()
+import { Camera } from 'expo-camera'
 
 const Vid = (props) => {
   const Algorithm = parseInt(props.Algorithm, 10)
+  const Plat = props.Platform
   console.log('Algorithm: ' + Algorithm)
-  const videoRef = useRef(null)
-  const photoRef = useRef(null)
-  const constraints = {
-    video: {
-      facingMode: 'environment',
-      framerate: { ideal: 60, max: 120 },
-      width: { ideal: 4096 },
-      height: { ideal: 2160 } //kind of hacky but uses ideal to force the video to display in the camera's max resolution
-    }
-  }
-  // TODO: method to change constraints facingMode: user or environment
-  useEffect(() => {
-    getVideo(constraints)
-  }, [videoRef])
 
-  const getVideo = (constraints) => {
-    navigator.mediaDevices.getUserMedia =
-      navigator.mediaDevices.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia ||
-      navigator.oGetUserMedia ||
-      navigator.msGetUserMedia // for compatibility's sake, if navigator.mediaDevices.getUserMedia is not supported, try older versions
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then((stream) => {
-        let video = videoRef.current
-        video.srcObject = stream
-        video.play()
-      })
-      .catch((err) => {
-        console.log(err.name)
-      })
-  }
-
-  const Paint = (alg) => {
-    let video = videoRef.current
-    let photo = photoRef.current
-    let ctx = photo.getContext('2d')
-    const width = video.videoWidth
-    const height = video.videoHeight
-    photo.width = width
-    photo.height = height
-
-    return setInterval(() => {
-      ctx.drawImage(video, 0, 0, width, height)
-      let pixels = ctx.getImageData(0, 0, width, height)
-      let l = pixels.data.length / 4
-      for (var i = 0; i < l; i++) {
-        const red = i * 4
-        const green = i * 4 + 1
-        const blue = i * 4 + 2
-        let newColors = Shift(
-          alg,
-          pixels.data[red],
-          pixels.data[green],
-          pixels.data[blue]
-        )
-        pixels.data[red] = newColors[0]
-        pixels.data[green] = newColors[1]
-        pixels.data[blue] = newColors[2]
+  if (Plat != 'Mobile') {
+    //Web
+    const constraints = {
+      video: {
+        facingMode: 'environment',
+        framerate: { ideal: 60, max: 120 },
+        width: { ideal: 4096 },
+        height: { ideal: 2160 } //kind of hacky but uses ideal to force the video to display in the camera's max resolution
       }
+    }
+    const videoRef = useRef(null)
+    const photoRef = useRef(null)
+    useEffect(() => {
+      getVideo(constraints)
+    }, [videoRef])
 
-      ctx.putImageData(pixels, 0, 0)
-    }, 1 / video.framerate) //interval of 16 = 60 fps, good baseline
-  }
+    const getVideo = (constraints) => {
+      navigator.mediaDevices.getUserMedia =
+        navigator.mediaDevices.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.oGetUserMedia ||
+        navigator.msGetUserMedia // for compatibility's sake, if navigator.mediaDevices.getUserMedia is not supported, try older versions
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then((stream) => {
+          let video = videoRef.current
+          video.srcObject = stream
+          video.play()
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
 
-  return (
-    <View>
-      <video
-        onCanPlay={() => Paint(Algorithm)}
-        ref={videoRef}
-        style={
-          { display: 'none' } //extremely inefficient! TODO: find a way around this
+    const Paint = (alg) => {
+      let video = videoRef.current
+      let photo = photoRef.current
+      let ctx = photo.getContext('2d')
+      const width = video.videoWidth
+      const height = video.videoHeight
+      photo.width = width
+      photo.height = height
+
+      return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height)
+        let pixels = ctx.getImageData(0, 0, width, height)
+        let l = pixels.data.length / 4
+        for (var i = 0; i < l; i++) {
+          const red = i * 4
+          const green = i * 4 + 1
+          const blue = i * 4 + 2
+          let newColors = Shift(
+            alg,
+            pixels.data[red],
+            pixels.data[green],
+            pixels.data[blue]
+          )
+          pixels.data[red] = newColors[0]
+          pixels.data[green] = newColors[1]
+          pixels.data[blue] = newColors[2]
         }
-      />
-      <canvas ref={photoRef} />
-    </View>
-  )
+
+        ctx.putImageData(pixels, 0, 0)
+      }, 1 / video.framerate) //interval of 16 = 60 fps, good baseline
+    }
+
+    return (
+      <View>
+        <video
+          ref={videoRef}
+          onCanPlay={() => Paint(Algorithm)}
+          style={
+            { display: 'none' } //extremely inefficient! TODO: find a way around this
+          }
+        />
+        <canvas ref={photoRef} />
+      </View>
+    )
+  } else {
+    //Mobile:
+    const [hasPermission, setHasPermission] = useState(null)
+    const [type, setType] = useState(Camera.Constants.Type.back)
+
+    useEffect(() => {
+      ;(async () => {
+        const { status } = await Camera.requestPermissionsAsync()
+        setHasPermission(status === 'granted')
+      })()
+    }, [])
+
+    if (hasPermission === null) {
+      return <View />
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>
+    }
+    return (
+      <View>
+        <Camera type={type}></Camera>
+      </View>
+    )
+  }
 }
 
 export default Vid
